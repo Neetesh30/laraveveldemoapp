6de52d5994737f99a01d17d13f056b883bd32f5e
Neetesh30 <neetesh652@gmail.com>
2021-09-07 18:14:29 +0530

    search doctor test2

--- app/Http/Controllers/DoctorScheduleAndBook/DoctorSearchController.php
+++ app/Http/Controllers/DoctorScheduleAndBook/DoctorSearchController.php
@@ -64,7 +64,7 @@ class DoctorSearchController extends Controller
                 'app_contactno',
             ]);                               
 
-        return view('welcome',['DoctorsList' => $doctorsdata,
+        return view('dashboard.DoctorScheduleAndBook.searchdoctor',['DoctorsList' => $doctorsdata,
                                         'rowcount' => $Count,'specialitylist'=>$specialitylist,
                                         'adminappdatas'=>$adminappdatas
                                     ]);
